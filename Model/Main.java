cc4bd44e3e5f4599836d6a748f6555567d3e9ee3
luca-moraes <lcansd.lm@gmail.com>
2023-09-27 21:36:41 -0300

    main do java

---
+++ Model/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String args[]){
+        String siteUrl = "http://legendas.tv/";
+
+        FileDownloader fDownloader = new FileDownloader(siteUrl);
+
+        JsoupParser jParser = new JsoupParser(fDownloader.getFilePath());
+
+        // HtmlParser hParser = new HtmlParser(fDownloader.getFilePath());
+
+        fDownloader.downloadImgs(jParser.getImageList());
+        fDownloader.downloadLinks(jParser.getLinkList());
+        fDownloader.downloadScripts(jParser.getScriptList());
+    }
+}
